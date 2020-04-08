d9e49015a4ddc2d0b75a2edb719c368796d60211
Ana-Stefania Gheorghe <31079035+a-gheorghe@users.noreply.github.com>
2020-04-08 16:09:16 -0400

    Add-edit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div> edit edit edit </div>
     </div>
   );
 }
